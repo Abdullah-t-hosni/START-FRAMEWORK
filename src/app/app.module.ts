0fc72dd34b3e51e6ca6609d1920872099e915a34
Abdullah-t-hosni <abdullah.taha574@gmail.com>
2024-02-11 16:20:20 +0200

    upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+
+import { NotfoundComponent } from './Notfound/Notfound.component';
+import { FooterComponent } from './Footer/Footer.component';
+
+@NgModule({
+  declarations: [						
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    AboutComponent,
+    ContactComponent,
+      PortfolioComponent,
+      NotfoundComponent,
+      FooterComponent
+   ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
